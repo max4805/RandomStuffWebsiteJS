8946c1be25881428b0a0016ca138f739740d63b8
max4805 <max480@free.fr>
2021-04-29 17:49:51 +0200

    Set up connect-history-api-fallback on Express

--- api/app.js
+++ api/app.js
@@ -1,10 +1,15 @@
-let express = require('express');
-let path = require('path');
-let logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const history = require('connect-history-api-fallback');
 
 let assetsRouter = require('./routes/assets');
 
 let app = express();
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(history());
+}
 
 app.use(logger('dev'));
 app.use(express.json());
