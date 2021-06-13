451bd28b3358ce5e7e056c3ddb5420e088ac400a
max4805 <max480@free.fr>
2021-06-13 12:28:29 +0200

    Added worst case test to wipe converter tests

--- api/tests/wipes.test.js
+++ api/tests/wipes.test.js
@@ -7,6 +7,13 @@ test('should convert a complex wipe to triangles', async() => {
     expect(triangles.length > 1000).toBe(true);
 });
 
+test('should convert worst case to triangles', async() => {
+    jest.setTimeout(60000);
+    const triangles = await wipes.convertWipeToTriangles('tests/assets/checkers.png');
+    expect(triangles).not.toBeNull();
+    expect(triangles.length).toBe(2073600);
+});
+
 test('should convert a black wipe to 2 triangles', async() => {
     const triangles = await wipes.convertWipeToTriangles('tests/assets/testwipe_black.png');
     expect(triangles).toHaveLength(2);
