135f73b26fccc93ef877652b99e151106fcd9ca1
max4805 <max480@free.fr>
2022-05-27 20:50:09 +0200

    Upgrade to Vue 3 with vue-compat

--- front/vue.config.js
+++ front/vue.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   productionSourceMap: false,
+  chainWebpack: (config) => {
+    config.resolve.alias.set("vue", "@vue/compat");
+
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2,
+            },
+          },
+        };
+      });
+  },
 };
